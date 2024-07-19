bc71559b18ba95dc543c3b3cd8b425b16076a91c
kak sophat <kaksophatt@gmail.com>
2024-07-19 08:51:01 +0700

    update context

--- src/Context/Shopcontext.jsx
+++ src/Context/Shopcontext.jsx
@@ -1,44 +1,47 @@
 import { createContext, useEffect, useState } from "react";
 
+export const ShopContext = createContext(null);
 
-export const  ShopContext= createContext(null)
- const getCart=()=>{
-       let Cart = {};
-       for(let index =0; index < 300+1; index++){
-        Cart[index]=0;
-       }
-       return Cart;
- }
-const ShopContextprovider= (props)=>{
-   const [all_product,set_allproduct] = useState([])
+const getCart = () => {
+    let Cart = {};
+    for (let index = 0; index < 300 + 1; index++) {
+        Cart[index] = 0;
+    }
+    return Cart;
+};
+
+const ShopContextProvider = (props) => {
+    const [all_product, set_allproduct] = useState([]);
     const [cartitems, setcartiems] = useState(getCart());
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://reactjs-e-comer-backend.onrender.com/allproduct')
-        .then((res)=>res.json())
-        .then((data)=>{set_allproduct(data),console.log(set_allproduct(data));})
-        
-        if(localStorage.getItem('Auth-Token')){
-            fetch('https://localhost:3000/getcart', {
-                      method: 'POST',
-                        headers: {
-                            Accept: 'application/form-data',
-                            'Content-Type': 'application/json',
-                            'Auth-Token': `${localStorage.getItem('Auth-Token')}` // Common practice to use Bearer token
-                        },
-                        body: ""
-                    })
-                    .then((res) => res.json())
-                    .then((data) => {setcartiems(data)})
-                 }
+            .then((res) => res.json())
+            .then((data) => {
+                set_allproduct(data);
+            });
 
-       
-  } ,[])
+        if (localStorage.getItem('Auth-Token')) {
+            fetch('https://reactjs-e-comer-backend.onrender.com/getcart', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/form-data',
+                    'Content-Type': 'application/json',
+                    'Auth-Token': `${localStorage.getItem('Auth-Token')}` // Common practice to use Bearer token
+                },
+                body: ""
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setcartiems(data);
+                });
+        }
+    }, []);
 
-    const addtocart=(itemid)=>{
-        setcartiems((prev)=>({...prev,[itemid]:prev[itemid]+1}));
+    const addtocart = (itemid) => {
+        if (localStorage.getItem('Auth-Token')) {
+            setcartiems((prev) => ({ ...prev, [itemid]: prev[itemid] + 1 }));
 
-        if (localStorage.getItem('Auth-Token')) { // Adjusted key name to remove space
             fetch('https://reactjs-e-comer-backend.onrender.com/addtocart', {
                 method: 'POST',
                 headers: {
@@ -48,18 +51,18 @@ const ShopContextprovider= (props)=>{
                 },
                 body: JSON.stringify({ 'itemid': itemid })
             })
-            .then((res) => res.json())
-            .then((data) => console.log(data))      
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+        } else {
+            alert("Please create an account before adding items to the cart.");
         }
-        else{
-            alert("create an account befor buy product")
-        }
-        
-    }
-    const removecart=(itemid)=>{
-        setcartiems((prev)=>({...prev,[itemid]:prev[itemid]-1}))
-        if(localStorage.getItem('Auth-Token')){
-            fetch('https://https://reactjs-e-comer-backend.onrender.com/removecart', {
+    };
+
+    const removecart = (itemid) => {
+        if (localStorage.getItem('Auth-Token')) {
+            setcartiems((prev) => ({ ...prev, [itemid]: prev[itemid] - 1 }));
+
+            fetch('https://reactjs-e-comer-backend.onrender.com/removecart', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/form-data',
@@ -68,38 +71,39 @@ const ShopContextprovider= (props)=>{
                 },
                 body: JSON.stringify({ 'itemid': itemid })
             })
-            .then((res) => res.json())
-            .then((data) => console.log(data))      
+                .then((res) => res.json())
+                .then((data) => console.log(data));
         }
-    }
-    const gettotalcart= ()=>{
-        var totalamout = 0;
-        for (const item in cartitems){
-            if(cartitems[item]>0){
-                let iteminfo = all_product.find((product)=>product.id===Number(item))
+    };
+
+    const gettotalcart = () => {
+        let totalamout = 0;
+        for (const item in cartitems) {
+            if (cartitems[item] > 0) {
+                let iteminfo = all_product.find((product) => product.id === Number(item));
                 totalamout += iteminfo.new_price * cartitems[item];
             }
         }
         return totalamout;
-    }
-    const gettotalcartitem=()=>{
-        let totalitem=0;
-        for(const item in cartitems){
-            if(cartitems[item]>0){
-                totalitem+=cartitems[item]
+    };
+
+    const gettotalcartitem = () => {
+        let totalitem = 0;
+        for (const item in cartitems) {
+            if (cartitems[item] > 0) {
+                totalitem += cartitems[item];
             }
         }
         return totalitem;
-    }
-    const Contextvalue= {all_product,cartitems, addtocart,removecart,gettotalcart,gettotalcartitem}
+    };
 
-    return(
-        <ShopContext.Provider value={Contextvalue} >           
+    const Contextvalue = { all_product, cartitems, addtocart, removecart, gettotalcart, gettotalcartitem };
+
+    return (
+        <ShopContext.Provider value={Contextvalue}>
             {props.children}
         </ShopContext.Provider>
+    );
+};
 
-    )
-
-}
-
-export default ShopContextprovider
+export default ShopContextProvider;
